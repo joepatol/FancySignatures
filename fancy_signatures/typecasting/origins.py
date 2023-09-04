78f389066053831d6ba469fc7cfe3f1e7fe44690
Joep Atol <jatol@ilionx.com>
2023-09-04 17:06:05 +0200

    small fix, more docs

--- fancy_signatures/typecasting/origins.py
+++ fancy_signatures/typecasting/origins.py
@@ -4,14 +4,15 @@ from ..core.interface import TypeCaster
 
 
 class OriginsTypeCaster(TypeCaster[Any]):
-    """Typecaster for type hints that don't have an origin
-    e.g. int, str or a user defined type that doesn't inherit from typing.Generic 
-    or is not subscipted
+    """Default typecaster. If no TypeCaster implementation is found this is the fallback.
+    
+    Check if the parameter value is a dict, tuple or list. If so try to unpack.
+    Otherwise call `self._type` with the given parameter.
     """
     def validate(self, param_value: Any) -> bool:
         return isinstance(param_value, self._type)
     
-    def cast(self, param_value: Any) -> int:
+    def cast(self, param_value: Any) -> Any:
         if isinstance(param_value, dict):
             return self._type(**param_value)
         if isinstance(param_value, (tuple, list)):
