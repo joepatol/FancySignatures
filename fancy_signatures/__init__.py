d0be70a72a5db2ff21ff96e73f8b0aeec5fdbd39
Joep Atol <jatol@ilionx.com>
2023-09-27 10:53:44 +0200

    add to_dict to exc group, api tests

--- fancy_signatures/__init__.py
+++ fancy_signatures/__init__.py
@@ -2,6 +2,6 @@
 .. include:: ../README.md
 """
 
-from .api import arg, validate  # noqa
+from .api import argument, validate  # noqa
 from .core.interface import TypeCaster, Validator, Default  # noqa
 from .core.empty import is_empty  # noqa
--- fancy_signatures/exceptions.py
+++ fancy_signatures/exceptions.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
 from typing import Any
 
 
 class ValidationError(Exception):
+    """Main validation error class"""
+
     def __init__(self, message: str, param: str | list[str]) -> None:
         if isinstance(param, list):
             msg = f"Parameters '{param}' are invalid. {message}."
@@ -11,22 +15,43 @@ class ValidationError(Exception):
 
 
 class TypeCastError(TypeError):
+    """Error raised when typecasting failed"""
+
     def __init__(self, expected_type: type | tuple[Any, ...], extra_info: str | None = None) -> None:
         super().__init__(f"Couldn't cast to correct type: {expected_type}. {extra_info if extra_info else ''}")
 
 
 class UnCastableType(TypeCastError):
+    """Error raised when typecasting is not possible for a type"""
+
     def __init__(self, expected_type: type | tuple[Any, ...]) -> None:
-        super().__init__(expected_type, extra_info="It's impossible to a type of: `{expected_type}`")
+        super().__init__(expected_type, extra_info="It's impossible to cast a type of: `{expected_type}`")
 
 
 class ValidatorFailed(ValueError):
+    """Error raised when a validator fails"""
+
     pass
 
 
 class TypeValidationError(TypeError):
+    """Error raised when type validation fails"""
+
     pass
 
 
 class ValidationErrorGroup(ExceptionGroup, ValidationError):
-    pass
+    """Main error group class"""
+
+    def to_dict(self) -> dict[str, Any]:
+        return _exception_group_to_dict(self)
+
+
+def _exception_group_to_dict(exc_group: ValidationErrorGroup) -> dict[str, Any]:
+    exc_list: list[str | dict[str, str]] = []
+    for exc_or_exc_group in exc_group.exceptions:
+        if isinstance(exc_or_exc_group, ValidationErrorGroup):
+            exc_list.append(_exception_group_to_dict(exc_or_exc_group))
+        else:
+            exc_list.append(str(exc_or_exc_group))
+    return {str(exc_group): exc_list}
