d6481e28a79fe001ccb24199dca604f86734e4c2
Joep Atol <jatol@ilionx.com>
2023-09-27 14:48:45 +0200

    new test release

--- fancy_signatures/__init__.py
+++ fancy_signatures/__init__.py
@@ -6,3 +6,4 @@ from .api import argument, validate  # noqa
 from .core.interface import TypeCaster, Validator, Default  # noqa
 from .core.empty import is_empty  # noqa
 from .typecasting.handlers import register_typecaster, unregister_typecaster, unregister_strict_typecaster  # noqa
+from .settings import set as adjust_setting, reset as reset_settings  # noqa
