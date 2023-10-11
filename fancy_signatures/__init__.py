47e9639cfa10e81ca87641499bb73f91d1416ef4
Joep Atol <jatol@ilionx.com>
2023-10-11 13:46:21 +0200

    add __version__.py

--- fancy_signatures/__init__.py
+++ fancy_signatures/__init__.py
@@ -7,3 +7,4 @@ from .core.interface import TypeCaster, Validator, Default  # noqa
 from .core.empty import is_empty  # noqa
 from .typecasting.handlers import register_typecaster, unregister_typecaster, unregister_strict_typecaster  # noqa
 from .settings import set as adjust_setting, reset as reset_settings  # noqa
+from .__version__ import __version__  # noqa
